de7c821f707357f08bbed15a90f354d3dcc3593c
Jeffrey Melloy <jmelloy@gmail.com>
2025-06-20 11:30:00 -0700

    update dump script

--- dump/mage.py
+++ dump/mage.py
@@ -6,6 +6,189 @@ import datetime
 from json import JSONEncoder
 import re
 import gzip
+from convert_to_xmp import create_xmp_metadata
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+l = logging.getLogger("urllib3")
+l.setLevel(logging.WARNING)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class DateTimeEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+
+
+def wrap(session, url, data={}, method="POST"):
+    logger.info(f"Calling {method} {url}")
+    start = datetime.datetime.now()
+
+    if method.upper() == "GET":
+        resp = session.get(f"{url}", params=data)
+    elif method.upper() == "POST":
+        resp = session.post(
+            f"{url}",
+            data=data,
+        )
+
+    end = datetime.datetime.now()
+
+    logger.info(f""" --> {resp.status_code} - {end - start}""")
+
+    # logger.debug(resp.text)
+
+    if resp.status_code > 205:
+        logger.warning(resp.text)
+
+    return resp.content, resp
+    
+
+
+def extract_braces(html_content):
+    open = 0
+    r = []
+    output = []
+    for el in html_content:
+        if el == "{":
+            open += 1
+        if open > 0:
+            r.append(el)
+        if el == "}":
+            open -= 1
+        if open == 0 and r:
+            output.append("".join(r))
+            r = []
+    return output
+
+
+def yield_images():
+    cookie = "ph_phc_lBVvvz084lS4XFbPqGf38TGEa6HnGhmgVHcf4f0NNuX_posthog=%7B%22distinct_id%22%3A%22dgGk4FOcMSf5KbIBqkQAteJolLB3%22%2C%22%24sesid%22%3A%5B1748822103918%2C%2201972de8-9dca-7ac0-a78a-59c37dfc5db9%22%2C1748821908938%5D%2C%22%24epp%22%3Atrue%2C%22%24initial_person_info%22%3A%7B%22r%22%3A%22%24direct%22%2C%22u%22%3A%22https%3A%2F%2Fwww.mage.space%2F%22%7D%7D; __session=eyJhbGciOiJSUzI1NiIsImtpZCI6Ik4yQlkwQSJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9tYWdlZG90c3BhY2UiLCJuYW1lIjoiSmVmZnJleSBNZWxsb3kiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSkV1TjNHamtHRWZucG9zNFlXcVhkdHB6cUxTbi0wb3dZZUpDVmhZSURpbEFcdTAwM2RzOTYtYyIsInN0cmlwZVJvbGUiOiJwcm9fcGx1cyIsImF1ZCI6Im1hZ2Vkb3RzcGFjZSIsImF1dGhfdGltZSI6MTc0NzE2NjcwNCwidXNlcl9pZCI6ImRnR2s0Rk9jTVNmNUtiSUJxa1FBdGVKb2xMQjMiLCJzdWIiOiJkZ0drNEZPY01TZjVLYklCcWtRQXRlSm9sTEIzIiwiaWF0IjoxNzQ4ODIyMTAxLCJleHAiOjE3NDg5MDg1MDEsImVtYWlsIjoiam1lbGxveUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwMzU0MTc1Mjk2MzYyNjY0ODQzMCJdLCJlbWFpbCI6WyJqbWVsbG95QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.NR7X8VcLXkYlkz4ltW-yD99rWxinFW9oIOdZZK-kYUxn_y8ScpKuXIqUsn49WGIzacLXGtRUJrBrMdaKg7m3AvOAkA8OV8ljosNb6DDycEnzdp_TMl4-iyf_05QNKxdzCrBMLYJTD7JiiG9KVtIxzVGrYy9DOG3377YEoGvaID3HO8JPQYv7QaROFZVsgAzr01JBKbl7V8Oxp_FeuSzqFyjvAbQ7hIqkto--3Epmn_vpVy9rxJJeIoheP7C-fVIJqNaoaXprxA28JeRgAUxmZdnU3-YQPXbLCaQeB-pkdC4h3RJFzigFDBSkZ0mtiIAZuBN83In9zkGiXVPDM5Te0A; cf_clearance=sdjQDJRnIWpVTwMzq51dlsZydDFgAnTMqUI7Ay9vXkg-1748821909-1.2.1.1-m55SVLynj9WmQBgRlhW0odJ5XVjo_sjdmRsYjgUXyKiq7hLvoI_SMlY4SEUYfcBKni9xCfyZ5zQsA3y_k8YmEDjq8RUErDcV_PaWhtzdmvKcEEj5bivtawse7Q52ZOYs7f7qMGdDlAtynWWhMNLqkqjdxpag61m9zAQF3onO51hj6one_W8HalqVVs5n2yoHJOLTZ2TWB5ejlyR_QLiXFvUKKlkE.pNwZLq3JWcNAUT0qPYmW.s7uOZ2Sc3AF3czLRQFHfocxKK8gUTsZeaNXzGx6O3Tv.Zf3dIMavnkPRmEsWRBB8Sj_k.6a2AV.Lngug2MKDtmEwjVM4MdfkqGWpBKNjHRFX19xyVqYvH055s; _iidt=EU88TS7T3Nqc25zngk6fq+6Dt9ScFjYSsR7cjMB8+GvGi8NY5HFftA4dbq6HOj+/GoKY9EPuSXynSydN0rnBP4GVyKGmPFk3be1vBnk="
+
+    request = ["dgGk4FOcMSf5KbIBqkQAteJolLB3", 100, 0, {
+        "orderBy": "desc",
+        "prompt": "$undefined",
+        "type": "$undefined",
+        "tag": "$undefined"
+    }]
+
+
+    session = requests.Session()
+    session.cookies.update(
+        {
+            "cookie": cookie,
+        }
+    )
+    session.headers.update(
+        {
+            "content-type": "text/plain",
+            "referer": "https://www.mage.space/u/jmelloy",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1.1 Safari/605.1.15",
+            "accept": "text/x-component",
+        }
+    )
+    url = "https://www.mage.space/u/jmelloy"
+
+    seen = True
+    offset = 0
+    while seen:
+        seen = False
+        request[2] = offset
+        print(request)
+        text, _ = wrap(session, url, data=json.dumps(request))
+
+        potential_json = extract_braces(text.decode("utf-8"))
+        for json_str in sorted(potential_json, key=len, reverse=True):
+            if seen:
+                continue
+            try:
+                json_str = re.sub(r'"embedding":\S+\[.*?\],?', '', json_str.replace('\\"', '"')).replace(",}", "}")
+                data = json.loads(json_str.replace('\\"', '"'))
+                
+
+                if "uid" in data and "children" in data:
+                    images = data["children"][-1]["creations"]
+                    logger.info(
+                        f"Found {len(images)} images from {images[0]['created_at']} to {images[-1]['created_at']}"
+                    )
+                    for image in data["children"][-1]["creations"]:
+                        image["created_at"] = image["created_at"].replace("$D", "")
+                        image["updated_at"] = image["updated_at"].replace("$D", "")
+                        yield image
+                        seen = True
+
+            except json.JSONDecodeError as e:
+                print(json_str, e)
+
+        offset += 100
+
+def yeild_from_file():
+    file = "mage_raw.txt"
+    with open(file) as F:
+        r = F.read()
+    
+    for row in extract_braces(r):
+        data = json.loads(row)
+        if "creations" not in data:
+            continue
+        for img in data["creations"]:
+            if img:
+                yield img
+            
+def main():
+    seen = set()
+    for image in yeild_from_file():
+        if image["id"] in seen:
+            continue
+            # raise Exception("Already seen")
+        seen.add(image["id"])
+        prompt = (image.get("concept_override") or {}).get("prompt", "")
+        if not prompt:
+            prompt = (image.get("architecture_config") or {}).get("prompt", "")
+
+        print(
+            image["created_at"],
+            image["url"].split("/")[-1],
+            prompt
+        )
+
+        filename = os.path.join(
+            "output",
+            "mage.space",
+            "jmelloy",
+            image["created_at"].split("T")[0].replace("-", "/"),
+            image["id"],
+            image["url"].split("/")[-1],
+        )
+
+        if not os.path.exists(os.path.dirname(filename)):
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        if not os.path.exists(filename):
+            with open(filename, "wb") as f:
+                f.write(requests.get(image["url"]).content)
+
+        with open(os.path.join(os.path.dirname(filename), "meta.json"), "w") as f:
+            f.write(json.dumps(image, indent=2))
+
+
+        sidecar = create_xmp_metadata(image)
+        with open(f"{filename}.xmp", "w") as f:
+            f.write(sidecar)
+
+if __name__ == "__main__":
+    main()
+import requests
+import logging
+import json
+import os
+import datetime
+from json import JSONEncoder
+import re
+import gzip
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
--- dump/walk.py
+++ dump/walk.py
@@ -5,6 +5,156 @@ import tkinter as tk
 import shutil
 import json
 
+def search_metadata(data: dict):
+    r = {}
+    for k, v in data.items():
+        if isinstance(v, dict):
+            r.update(search_metadata(v))
+        else:
+            if k == "prompt":
+                r["prompt"] = v
+            if k.startswith("created"):
+                r["created_at"] = v
+    return r
+
+def get_metadata(file):
+    data = {}
+    if os.path.exists(file + ".json"):
+        with open(file + ".json") as f:
+            data = json.load(f)
+    else:
+        folder = os.path.dirname(file)
+        files = [f for f in os.listdir(folder) if f.endswith(".json")]
+        for f in files:
+            filename = os.path.basename(f)
+            with open(os.path.join(folder, f)) as f:
+                content = f.read()
+                if filename in content:
+                    data = json.loads(content)
+
+    if data:
+        return search_metadata(data)
+
+    return None
+
+def get_images(directory):
+    supported_formats = (".png", ".jpg", ".jpeg", ".gif", ".bmp")
+    file_response = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(supported_formats):
+                metadata = get_metadata(os.path.join(root, file))
+                created_at = ''
+                prompt = ''
+                if metadata:
+                    created_at = metadata["created_at"]
+                    prompt = metadata["prompt"]
+                file_response.append((os.path.join(root, file), created_at, prompt))
+                    
+    file_response.sort(key=lambda x: x[1])
+    prev_prompt = None
+    for file, dt, prompt in file_response:
+        if prompt != prev_prompt:
+            print(dt, prompt)
+        yield file
+        prev_prompt = prompt
+
+
+def main(source_dir, dest_dir, log_file):
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    if os.path.exists(log_file):
+        with open(log_file, "r") as f:
+            shown_images = set(f.read().splitlines())
+    else:
+        shown_images = set()
+
+    images = get_images(source_dir)
+
+    root = tk.Tk()
+    root.title("Image Viewer")
+    image_iter = iter(images)
+
+    def update_shown_images(img):
+        with open(log_file, "a") as f:
+            f.write(img + "\n")
+        shown_images.add(img)
+
+    def show_next_image():
+        try:
+            image = next(image_iter)
+            while image in shown_images:
+                image = next(image_iter)
+
+
+            img = Image.open(image)
+            img = img.resize((800, 800))
+            root.title(f"Image Viewer - {os.path.basename(image)}")
+
+            photo = ImageTk.PhotoImage(img)
+            label.config(image=photo)
+            label.image = photo
+
+            def on_yes():
+                print(image)
+                shutil.copy(image, dest_dir)
+                update_shown_images(image)
+                show_next_image()
+
+            def on_no():
+                update_shown_images(image)
+                show_next_image()
+
+            yes_button.config(command=on_yes)
+            no_button.config(command=on_no)
+        except UnidentifiedImageError:
+            update_shown_images(image)
+            show_next_image()
+        except StopIteration:
+            root.quit()
+
+    image = next(image_iter)
+    img = Image.open(image)
+    img = img.resize((800, 800))
+    photo = ImageTk.PhotoImage(img)
+    label = tk.Label(root, image=photo)
+    label.pack()
+
+    yes_button = tk.Button(root, text="Yes")
+    yes_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+    def on_no():
+        update_shown_images(image)
+        show_next_image()
+
+    no_button = tk.Button(root, text="No")
+    no_button.pack(side=tk.RIGHT, padx=10, pady=10)
+    no_button.config(command=on_no)
+
+    def on_yes():
+        shutil.copy(image, dest_dir)
+        update_shown_images(image)
+        show_next_image()
+
+    yes_button.config(command=on_yes)
+
+    root.bind("<Left>", lambda event: yes_button.invoke())
+    root.bind("<Right>", lambda event: no_button.invoke())
+
+    show_next_image()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], os.path.join(sys.argv[1], ".log.txt"))
+import sys
+import os
+from PIL import Image, ImageTk, UnidentifiedImageError
+import tkinter as tk
+import shutil
+import json
+
 
 def get_images(directory):
     supported_formats = (".png", ".jpg", ".jpeg", ".gif", ".bmp")
